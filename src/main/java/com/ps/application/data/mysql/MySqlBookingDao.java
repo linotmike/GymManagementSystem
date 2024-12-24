d1d1fb09d99c60aaf6e9d0971ebea46ae4baa738
linotmike <128604219+linotmike@users.noreply.github.com>
2024-12-24 02:24:13 -0800

    added method to map books

--- src/main/java/com/ps/application/data/mysql/MySqlBookingDao.java
+++ src/main/java/com/ps/application/data/mysql/MySqlBookingDao.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class MySqlBookingDao extends MySqlBaseDao implements BookingDao {
     public MySqlBookingDao(DataSource dataSource) {
@@ -18,5 +19,18 @@ public class MySqlBookingDao extends MySqlBaseDao implements BookingDao {
     public Booking getByBookId(int bookingId) {
 
         return null;
+    }
+
+
+    public Booking mapBooks (ResultSet resultSet) throws SQLException{
+        int bookingId = resultSet.getInt("booking_id");
+        int classID = resultSet.getInt("class_id");
+        int memberId = resultSet.getInt("member_id");
+        int userId = resultSet.getInt("user_id");
+        int scheduleId = resultSet.getInt("schedule_id");
+        LocalDate bookingDate = resultSet.getDate("booking_date").toLocalDate();
+        Booking.Status status = Booking.Status.valueOf(resultSet.getString("status").toUpperCase());
+
+        return new Booking(bookingId,classID,memberId,userId,scheduleId,bookingDate,status);
     }
 }
