c30713ec990acff65b14b5f80c3cda7ff0183e42
linotmike <128604219+linotmike@users.noreply.github.com>
2024-12-31 12:35:00 -0800

    Added methods to profile dao interface to create,update and get profiles by user id

--- src/main/java/com/ps/application/data/ProfileDao.java
+++ src/main/java/com/ps/application/data/ProfileDao.java
@@ -1,4 +1,9 @@
 package com.ps.application.data;
 
+import com.ps.application.models.Profile;
+
 public interface ProfileDao {
+    Profile createProfile (Profile profile);
+    Profile getUserById(int profileId);
+    Profile updateProfile (int profileId, Profile profile);
 }
--- src/main/java/com/ps/application/data/mysql/MySqlProfileDao.java
+++ src/main/java/com/ps/application/data/mysql/MySqlProfileDao.java
@@ -1,6 +1,7 @@
 package com.ps.application.data.mysql;
 
 import com.ps.application.data.ProfileDao;
+import com.ps.application.models.Profile;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
@@ -9,5 +10,20 @@ import javax.sql.DataSource;
 public class MySqlProfileDao extends MySqlBaseDao implements ProfileDao {
     public MySqlProfileDao(DataSource dataSource) {
         super(dataSource);
+    }
+
+    @Override
+    public Profile createProfile(Profile profile) {
+        return null;
+    }
+
+    @Override
+    public Profile getUserById(int profileId) {
+        return null;
+    }
+
+    @Override
+    public Profile updateProfile(int profileId, Profile profile) {
+        return null;
     }
 }
