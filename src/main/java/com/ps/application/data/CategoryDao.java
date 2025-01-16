ee23708fd40aa869c2e305bc1b10cede31b831cd
linotmike <128604219+linotmike@users.noreply.github.com>
2025-01-16 02:38:41 -0800

    Added methods to the category interface and added map categories method to map over the categories

--- src/main/java/com/ps/application/data/CategoryDao.java
+++ src/main/java/com/ps/application/data/CategoryDao.java
@@ -1,4 +1,15 @@
 package com.ps.application.data;
 
+import com.ps.application.models.Category;
+
+import java.util.List;
+
 public interface CategoryDao {
-}
+    Category getCategoryById(int categoryId);
+    List<Category> getAllCategories ();
+    Category createCategories( Category category);
+    Category updateCategory (int categoryId, Category category);
+    Category deleteCategory(int categoryId);
+
+    }
+
--- src/main/java/com/ps/application/data/mysql/MySqlCategoryDao.java
+++ src/main/java/com/ps/application/data/mysql/MySqlCategoryDao.java
@@ -1,13 +1,53 @@
 package com.ps.application.data.mysql;
 
 import com.ps.application.data.CategoryDao;
+import com.ps.application.models.Category;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Component
 public class MySqlCategoryDao extends MySqlBaseDao implements CategoryDao {
     public MySqlCategoryDao(DataSource dataSource) {
         super(dataSource);
+    }
+
+    @Override
+    public Category getCategoryById(int categoryId) {
+        return null;
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return List.of();
+    }
+
+    @Override
+    public Category createCategories(Category category) {
+        return null;
+    }
+
+    @Override
+    public Category updateCategory(int categoryId, Category category) {
+        return null;
+    }
+
+    @Override
+    public Category deleteCategory(int categoryId) {
+        return null;
+    }
+    public Category mapCategories (ResultSet resultSet) throws SQLException{
+        int categoryId = resultSet.getInt("category_id");
+        String name = resultSet.getString("name");
+        String description = resultSet.getString("description");
+
+        return new Category(categoryId,name,description);
+
+
     }
 }
