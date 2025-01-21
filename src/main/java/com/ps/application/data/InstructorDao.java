fa84a249fb88bb8b6815ea9aae6a695992b1ca59
linotmike <128604219+linotmike@users.noreply.github.com>
2025-01-20 19:10:15 -0800

    Added implement methods to the InstructorDao

--- src/main/java/com/ps/application/data/InstructorDao.java
+++ src/main/java/com/ps/application/data/InstructorDao.java
@@ -1,4 +1,13 @@
 package com.ps.application.data;
 
+import com.ps.application.models.Instructors;
+
+import java.util.List;
+
 public interface InstructorDao {
+    List<Instructors> getAllInstructors();
+    Instructors getInstructorById (int instructorId);
+    Instructors createInstructors (Instructors instructors);
+    Instructors updateInstructors(int instructorId, Instructors instructors);
+    Instructors deleteInstructors (int instructorId);
 }
--- src/main/java/com/ps/application/data/mysql/MySqlInstructorDao.java
+++ src/main/java/com/ps/application/data/mysql/MySqlInstructorDao.java
@@ -1,12 +1,40 @@
 package com.ps.application.data.mysql;
 
 import com.ps.application.data.InstructorDao;
+import com.ps.application.models.Instructors;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.util.List;
+
 @Component
 public class MySqlInstructorDao extends MySqlBaseDao implements InstructorDao {
     public MySqlInstructorDao(DataSource dataSource) {
         super(dataSource);
+    }
+
+    @Override
+    public List<Instructors> getAllInstructors() {
+        return List.of();
+    }
+
+    @Override
+    public Instructors getInstructorById(int instructorId) {
+        return null;
+    }
+
+    @Override
+    public Instructors createInstructors(Instructors instructors) {
+        return null;
+    }
+
+    @Override
+    public Instructors updateInstructors(int instructorId, Instructors instructors) {
+        return null;
+    }
+
+    @Override
+    public Instructors deleteInstructors(int instructorId) {
+        return null;
     }
 }
